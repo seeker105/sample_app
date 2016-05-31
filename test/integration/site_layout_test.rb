db593fac4c735b05893da77228a790842d7580a9
Christopher Soden <karavol27@gmail.com>
2016-05-30 18:37:46 -0600

    Add test for the full_title helper method

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,5 +9,21 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+  end
+
+  test "signup page" do
+    get signup_path
+    page.has_text?("Sign up")
+  end
+
+  test "signup page full title" do
+    get signup_path
+    assert_select "title", full_title("Sign up")
+  end
+
+  test "full title helper" do
+    assert_equal full_title,         "Ruby on Rails Tutorial"
+    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial"
   end
 end
