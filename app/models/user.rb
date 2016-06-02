1978ffc57b63458395157f8b38aa9d3ed54f33fa
Christopher Soden <karavol27@gmail.com>
2016-06-02 09:22:26 -0600

    Add downcase email and password_digest to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  # attr_accessor :name, :email
-
+  before_save {self.email = email.downcase  }
+  has_secure_password
+  
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: {case_sensitive: false }
 end
